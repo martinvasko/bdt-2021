9aa9babccf9da8ffffe24c24a52462643209616b
Manuel Leithner <mleithner@sba-research.org>
2021-10-09 14:57:49 +0200

    Pythonized exercise 6

--- python/ingestion_schema_manipulation.py
+++ python/ingestion_schema_manipulation.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
 
 def setup_spark_session():
     '''Creates a session on a local master.'''
@@ -14,7 +15,11 @@ if __name__ == "__main__":
         print("Usage: ingestion_schema_manipulation <input.csv>", file=sys.stderr)
         sys.exit(-1)
 
+    # Setup
+
     spark = setup_spark_session()
+
+    # Ingestion (Exercise 5)
 
     df = read_file_source(spark, sys.argv[1])
 
@@ -22,5 +27,32 @@ if __name__ == "__main__":
     df.show(n=5)
     df.printSchema()
     print(f'We have {df.count()} records')
+
+    # Transformation (Exercise 6)
+
+    df = (df
+            .withColumn('county', lit('Wake'))
+            .withColumnRenamed('HSISID', 'datasetId')
+            .withColumnRenamed('NAME', 'name')
+            .withColumnRenamed('ADDRESS1', 'address1')
+            .withColumnRenamed('ADDRESS2', 'address2')
+            .withColumnRenamed('CITY', 'city')
+            .withColumnRenamed('STATE', 'state')
+            .withColumnRenamed('POSTALCODE', 'zip')
+            .withColumnRenamed('PHONENUMBER', 'tel')
+            .withColumnRenamed('RESTAURANTOPENDATE', 'dateStart')
+            .withColumnRenamed('FACILITYTYPE', 'type')
+            .withColumnRenamed('X', 'geoX')
+            .withColumnRenamed('Y', 'geoY')
+            .drop('OBJECTID')
+            .drop('PERMITID')
+            .drop('GEOCODESTATUS')
+            )
+
+
+    print('*** After transformation')
+    df.show(n=5)
+    df.printSchema()
+
 
     spark.stop()
