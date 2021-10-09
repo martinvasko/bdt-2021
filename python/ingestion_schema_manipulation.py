39a2df9bbd9745478a0cc6d15d15f6756a89fa62
Manuel Leithner <mleithner@sba-research.org>
2021-10-09 15:46:39 +0200

    Small code cleanup

--- python/ingestion_schema_manipulation.py
+++ python/ingestion_schema_manipulation.py
@@ -11,6 +11,38 @@ def read_file_source(spark, source):
     '''Reads a CSV file with header and stores it in a dataframe.'''
     return spark.read.format('csv').option('header', True).load(source)
 
+def transform_columns(df):
+    return (df
+            .withColumn('county', lit('Wake'))
+            .withColumnRenamed('HSISID', 'datasetId')
+            .withColumnRenamed('NAME', 'name')
+            .withColumnRenamed('ADDRESS1', 'address1')
+            .withColumnRenamed('ADDRESS2', 'address2')
+            .withColumnRenamed('CITY', 'city')
+            .withColumnRenamed('STATE', 'state')
+            .withColumnRenamed('POSTALCODE', 'zip')
+            .withColumnRenamed('PHONENUMBER', 'tel')
+            .withColumnRenamed('RESTAURANTOPENDATE', 'dateStart')
+            .withColumnRenamed('FACILITYTYPE', 'type')
+            .withColumnRenamed('X', 'geoX')
+            .withColumnRenamed('Y', 'geoY')
+            .drop('OBJECTID')
+            .drop('PERMITID')
+            .drop('GEOCODESTATUS')
+            )
+
+def add_id_column(df):
+    return (df
+            .withColumn('id', concat(
+                df.state,
+                lit('_'),
+                df.county,
+                lit('_'),
+                df.datasetId
+                )
+            )
+        )
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ingestion_schema_manipulation <input.csv>", file=sys.stderr)
@@ -31,41 +63,14 @@ if __name__ == "__main__":
 
     # Transformation (Exercise 6)
 
-    df = (df
-            .withColumn('county', lit('Wake'))
-            .withColumnRenamed('HSISID', 'datasetId')
-            .withColumnRenamed('NAME', 'name')
-            .withColumnRenamed('ADDRESS1', 'address1')
-            .withColumnRenamed('ADDRESS2', 'address2')
-            .withColumnRenamed('CITY', 'city')
-            .withColumnRenamed('STATE', 'state')
-            .withColumnRenamed('POSTALCODE', 'zip')
-            .withColumnRenamed('PHONENUMBER', 'tel')
-            .withColumnRenamed('RESTAURANTOPENDATE', 'dateStart')
-            .withColumnRenamed('FACILITYTYPE', 'type')
-            .withColumnRenamed('X', 'geoX')
-            .withColumnRenamed('Y', 'geoY')
-            .drop('OBJECTID')
-            .drop('PERMITID')
-            .drop('GEOCODESTATUS')
-            )
-
+    df = transform_columns(df)
 
     print('*** After transformation')
     df.show(n=5)
     df.printSchema()
 
     # Unique identifier transformation (Exercise 7)
-    df = (df
-            .withColumn('id', concat(
-                df.state,
-                lit('_'),
-                df.county,
-                lit('_'),
-                df.datasetId
-                )
-            )
-        )
+    df = add_id_column(df)
 
     # Exercise 8, I suppose
     print('*** After adding new unique ID')
@@ -86,6 +91,6 @@ class TestIngestTransform(unittest.TestCase):
     def test_ingest(self):
         # XXX TODO Adjust this to your environment
         path_csv_file = '../data/input/spark/Restaurants_in_Wake_County.csv'
-        df = read_file_source(self.spark, path_csv_file)
-        self.assertGreater(len(df.columns), 0)
-        self.assertGreaterEqual(df.count(), 0)
+        self.df = read_file_source(self.spark, path_csv_file)
+        self.assertGreater(len(self.df.columns), 0)
+        self.assertGreaterEqual(self.df.count(), 0)
