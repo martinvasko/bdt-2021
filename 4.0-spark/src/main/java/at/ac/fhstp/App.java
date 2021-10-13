09789865387c5ee91647a1251d62475d2564b75d
martinvasko <martin@expressflow.com>
2021-10-13 10:11:43 +0200

    Added launch config

--- 4.0-spark/src/main/java/at/ac/fhstp/App.java
+++ 4.0-spark/src/main/java/at/ac/fhstp/App.java
@@ -1,10 +1,6 @@
 package at.ac.fhstp;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.Level;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 
 /**
  * Approximate Pi!
@@ -12,14 +8,9 @@ import org.apache.spark.sql.Row;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Running Ingestion");
-        if (args.length == 0) {
-            System.out.println("No files provided.");
-            System.exit(0);
-        }
-        SparkSession spark = SparkSession.builder().appName("Approximate Pi").master("local[*]")
-                .getOrCreate();
-       ApproximatePi app = new ApproximatePi();
+        SparkSession spark = SparkSession.builder().appName("Approximate Pi").master("local[*]").getOrCreate();
+        ApproximatePi app = new ApproximatePi();
+        app.start(spark, 10);
         spark.stop();
     }
 }
